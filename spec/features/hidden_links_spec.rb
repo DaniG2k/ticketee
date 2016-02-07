d76350918f95dd69bf47457c6547f6c225c7ec69
Daniele Pestilli <dpestilli@funnelback.com>
2016-02-07 12:11:48 +0000

    Only managers and admins can delete tickets

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "Users can only see the appropriate links" do
-  let(:project) {FactoryGirl.create(:project)}
-  let(:user) {FactoryGirl.create(:user)}
-  let(:admin) {FactoryGirl.create(:user, :admin)}
+  let(:project) { FactoryGirl.create(:project) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:user, :admin) }
+  let(:ticket) { FactoryGirl.create(:ticket, project: project, author: user) }
 
   context 'non-admin users (project viewers)' do
     before do
@@ -23,6 +24,18 @@ RSpec.feature "Users can only see the appropriate links" do
       visit project_path(project)
       expect(page).not_to have_link 'Edit Project'
     end
+    scenario 'cannot see the New Ticket link' do
+      visit project_path(project)
+      expect(page).not_to have_link 'New Ticket'
+    end
+    scenario 'cannot see the Edit Ticket link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link 'Edit Ticket'
+    end
+    scenario 'cannot see the Delete Ticket link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link 'Delete Ticket'
+    end
   end
 
   context 'admin users' do
@@ -39,6 +52,18 @@ RSpec.feature "Users can only see the appropriate links" do
     scenario '' do
       visit project_path(project)
       expect(page).to have_link 'Edit Project'
+    end
+    scenario 'can see the New Ticket link' do
+      visit project_path(project)
+      expect(page).to have_link 'New Ticket'
+    end
+    scenario 'can see the Edit Ticket link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link 'Edit Ticket'
+    end
+    scenario 'can see the Delete Ticket link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link 'Delete Ticket'
     end
   end
 end
