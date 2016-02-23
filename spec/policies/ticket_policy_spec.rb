295a0249fba3dfbf83b56071fec8990e7be0f2a1
DaniG2k <byakugan.87@gmail.com>
2016-02-23 21:15:19 +0000

    Restrict tagging to only authorized users

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -15,6 +15,7 @@ describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
     context 'for viewers of the project' do
       before {assign_role!(user, :viewer, project)}
@@ -23,6 +24,7 @@ describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
     context 'for editors of the project' do
       before {assign_role!(user, :editor, project)}
@@ -36,6 +38,7 @@ describe TicketPolicy do
         it { should permit_action :update }
       end
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
     context 'for managers of the project' do
       before {assign_role!(user, :manager, project)}
@@ -44,6 +47,7 @@ describe TicketPolicy do
       it { should permit_action :update }
       it { should permit_action :destroy }
       it { should permit_action :change_state }
+      it { should permit_action :tag }
     end
     context 'for managers of other projects' do
       before {assign_role!(user, :manager, FactoryGirl.create(:project))}
@@ -52,6 +56,7 @@ describe TicketPolicy do
       it { should_not permit_action :update }
       it { should_not permit_action :destroy }
       it { should_not permit_action :change_state }
+      it { should_not permit_action :tag }
     end
     context 'for administrators' do
       let(:user) {FactoryGirl.create(:user, :admin)}
@@ -60,6 +65,7 @@ describe TicketPolicy do
       it { should permit_action :update }
       it { should permit_action :destroy }
       it { should permit_action :change_state }
+      it { should permit_action :tag }
     end
   end
 end
