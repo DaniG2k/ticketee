20557828a3cbff8d185dcbe63e2c8728ae24d9f1
Daniele Pestilli <dpestilli@funnelback.com>
2016-02-27 15:20:30 +0000

    Users can now click a tag's name to go to a page showing all tickets for it

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -3,13 +3,17 @@ require 'rails_helper'
 RSpec.feature 'Users can search for tikets matching specific criteria' do
   let(:user) {FactoryGirl.create(:user)}
   let(:project) {FactoryGirl.create(:project)}
+  let(:open) {State.create(name: 'Open', default: true)}
+  let(:closed) {State.create(name: 'Closed')}
+
   let!(:ticket_1) do
     FactoryGirl.create(
       :ticket,
       name: 'Create projects',
       project: project,
       author: user,
-      tag_names: 'iteration_1'
+      tag_names: 'iteration_1',
+      state: open
     )
   end
   let!(:ticket_2) do
@@ -18,7 +22,8 @@ RSpec.feature 'Users can search for tikets matching specific criteria' do
       name: 'Create users',
       project: project,
       author: user,
-      tag_names: 'iteration_2'
+      tag_names: 'iteration_2',
+      state: closed
     )
   end
 
@@ -34,6 +39,22 @@ RSpec.feature 'Users can search for tikets matching specific criteria' do
     within("#tickets") do
       expect(page).to have_link 'Create projects'
       expect(page).to_not have_link 'Create users'
+    end
+  end
+  scenario 'searching by state' do
+    fill_in 'Search', with: 'state:Open'
+    click_button 'Search'
+    within("#tickets") do
+      expect(page).to have_link 'Create projects'
+      expect(page).to_not have_link 'Create users'
+    end
+  end
+  scenario 'when clicking on a tag' do
+    click_link 'Create projects'
+    click_link 'iteration_1'
+    within("#tickets") do
+      expect(page).to have_content 'Create projects'
+      expect(page).to_not have_content 'Create users'
     end
   end
 end
