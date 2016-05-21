b37e2b6a625b6f2478adf4f7652b7c1c71db5d95
Daniele Pestilli <dpestilli@funnelback.com>
2016-05-21 15:04:58 +0100

    Fixed race condition in State#make_default\!

--- app/models/state.rb
+++ app/models/state.rb
@@ -4,8 +4,14 @@ class State < ActiveRecord::Base
   end
 
   def make_default!
-    State.update_all(default: false)
-    update!(default: true)
+    # Prevent race condition using database-level locks.
+    State.transaction do
+      all = State.where.not(id: id).lock(true).first
+      all.update_all(default: false) unless all.nil?
+      
+      current = State.where(id: id).lock(true).first
+      current.update!(default: true)
+    end
   end
 
   def to_s
