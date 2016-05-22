c73abd89574372504125970352ddfc31484d0e43
Daniele Pestilli <dpestilli@funnelback.com>
2016-05-22 16:29:17 +0100

    Added model spec for State#make_default\!

--- app/models/state.rb
+++ app/models/state.rb
@@ -6,7 +6,7 @@ class State < ActiveRecord::Base
   def make_default!
     # Prevent race condition using database-level locks.
     State.transaction do
-      State.update_all(default: false)
+      State.where.not(id: id).lock(true).update_all(default: false)
       State.where(id: id).lock(true).first.update!(default: true)
     end
   end
